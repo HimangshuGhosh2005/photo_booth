12590f1f905ab96699e431ae95e61c66e3d03bed
HimangshuGhosh2005 <628968hghosh@gmail.com>
2025-08-20 22:02:38 +0530

    update

--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ from .models import database
 
 # Create your views here.
 
+#post
 class photoupload(APIView):
     parser_classes=[FormParser,MultiPartParser]
     def post(self,req):
@@ -16,7 +17,7 @@ class photoupload(APIView):
             serial.save()
             metadata={'file_name':serial.instance.name, 'file_size': f"{round(serial.instance.url.size/1024,2)} KB", 'upload_Time':serial.instance.uploaded_At }
         return Response(metadata)
-    
+#get    
 class photo(APIView):
     def get(self,req):
         data=database.objects.all()
@@ -24,12 +25,12 @@ class photo(APIView):
         return Response(serialdata.data)   
 
 
-
+# getting the path of base directory
 import os
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent 
 
-
+#delete
 class delete(APIView):
     def delete(self,req):
         obj=database.objects.get(id=req.data['id'])
